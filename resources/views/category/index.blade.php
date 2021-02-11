bb1f56f6bf35ef6311e242985c735e1153941fe5
Yudi <madewahyudi22@gmail.com>
2021-02-11 16:36:03 +0800

    fix table-responsive view

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -10,43 +10,45 @@
             <i class="fa fa-fw fa-plus"></i>
             Add New
         </button>
-        <table id="datatables" class="table table-striped table-bordered table-responsive-xl">
-            <thead>
-                <tr class="text-center">
-                    <th>No.</th>
-                    <th class="text-nowrap">Category Name</th>
-                    <th class="text-nowrap">Category Slug</th>
-                    <th class="text-nowrap">Itinerary Count</th>
-                    <th class="text-nowrap">Added at</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($categories as $key => $category)
-                    <tr>
-                        <td class="text-center">{{ $key + 1 }}</td>
-                        <td>
-                            <a class="text-dark" href="{{ route('category.edit', $category) }}"> {{ $category->name }} </a>
-                        </td>
-                        <td>{{ $category->slug }}</td>
-                        <td class="text-center">{{ $category->itineraries()->count() }}</td>
-                        <td>{{ $category->updated_at->diffForHumans() }}</td>
-                        <td class="text-nowrap text-center">
-                            <a class="btn btn-primary btn-sm" href="{{ route('category.edit', $category) }}" title="Edit">
-                                <i class="fa fa-fw fa-pencil-alt"></i>
-                            </a>
-                            <form id="delete-form" class="d-inline-block m-0" action="{{ route('category.destroy', $category) }}" method="POST" title="Delete">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" onclick="confirm('Are you sure?') && $(this).parent().submit()" class="btn btn-sm btn-danger">
-                                    <i class="fa fa-fw fa-trash"></i>
-                                </button>
-                            </form>
-                        </td>
+        <div class="table-responsive">
+            <table id="datatables" class="table table-striped table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th>No.</th>
+                        <th class="text-nowrap">Category Name</th>
+                        <th class="text-nowrap">Category Slug</th>
+                        <th class="text-nowrap">Itinerary Count</th>
+                        <th class="text-nowrap">Added at</th>
+                        <th>Action</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $key => $category)
+                        <tr>
+                            <td class="text-center">{{ $key + 1 }}</td>
+                            <td>
+                                <a class="text-dark" href="{{ route('category.edit', $category) }}"> {{ $category->name }} </a>
+                            </td>
+                            <td>{{ $category->slug }}</td>
+                            <td class="text-center">{{ $category->itineraries()->count() }}</td>
+                            <td>{{ $category->updated_at->diffForHumans() }}</td>
+                            <td class="text-nowrap text-center">
+                                <a class="btn btn-primary btn-sm" href="{{ route('category.edit', $category) }}" title="Edit">
+                                    <i class="fa fa-fw fa-pencil-alt"></i>
+                                </a>
+                                <form id="delete-form" class="d-inline-block m-0" action="{{ route('category.destroy', $category) }}" method="POST" title="Delete">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" onclick="confirm('Are you sure?') && $(this).parent().submit()" class="btn btn-sm btn-danger">
+                                        <i class="fa fa-fw fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </section>
 @endsection
 @include('category.quick-add')
--- resources/views/district/index.blade.php
+++ resources/views/district/index.blade.php
@@ -10,45 +10,47 @@
             <i class="fa fa-fw fa-plus"></i>
             Add New
         </button>
-        <table id="datatables" class="table table-striped table-bordered table-responsive-xl">
-            <thead>
-                <tr class="text-center">
-                    <th>No.</th>
-                    <th class="text-nowrap">District Name</th>
-                    <th class="text-nowrap">District Slug</th>
-                    <th class="text-nowrap">Itinerary Count</th>
-                    <th class="text-nowrap">Added at</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($districts as $key => $district)
-                    <tr>
-                        <td class="text-center">{{ $key + 1 }}</td>
-                        <td>
-                            <a class="text-dark" href="{{ route('district.edit', $district) }}">
-                                {{ $district->name }}
-                            </a>
-                        </td>
-                        <td>{{ $district->slug }}</td>
-                        <td class="text-center">{{ $district->itineraries()->count() }}</td>
-                        <td>{{ $district->updated_at->diffForHumans() }}</td>
-                        <td class="text-nowrap text-center">
-                            <a class="btn btn-primary btn-sm" href="{{ route('district.edit', $district) }}" title="Edit">
-                                <i class="fa fa-fw fa-pencil-alt"></i>
-                            </a>
-                            <form id="delete-form" class="d-inline-block m-0" action="{{ route('district.destroy', $district) }}" method="POST" title="Delete">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" onclick="confirm('Are you sure?') && $(this).parent().submit()" class="btn btn-sm btn-danger">
-                                    <i class="fa fa-fw fa-trash"></i>
-                                </button>
-                            </form>
-                        </td>
+        <div class="table-responsive">
+            <table id="datatables" class="table table-striped table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th>No.</th>
+                        <th class="text-nowrap">District Name</th>
+                        <th class="text-nowrap">District Slug</th>
+                        <th class="text-nowrap">Itinerary Count</th>
+                        <th class="text-nowrap">Added at</th>
+                        <th>Action</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($districts as $key => $district)
+                        <tr>
+                            <td class="text-center">{{ $key + 1 }}</td>
+                            <td>
+                                <a class="text-dark" href="{{ route('district.edit', $district) }}">
+                                    {{ $district->name }}
+                                </a>
+                            </td>
+                            <td>{{ $district->slug }}</td>
+                            <td class="text-center">{{ $district->itineraries()->count() }}</td>
+                            <td>{{ $district->updated_at->diffForHumans() }}</td>
+                            <td class="text-nowrap text-center">
+                                <a class="btn btn-primary btn-sm" href="{{ route('district.edit', $district) }}" title="Edit">
+                                    <i class="fa fa-fw fa-pencil-alt"></i>
+                                </a>
+                                <form id="delete-form" class="d-inline-block m-0" action="{{ route('district.destroy', $district) }}" method="POST" title="Delete">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" onclick="confirm('Are you sure?') && $(this).parent().submit()" class="btn btn-sm btn-danger">
+                                        <i class="fa fa-fw fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </section>
 @endsection
 @include('district.quick-add')

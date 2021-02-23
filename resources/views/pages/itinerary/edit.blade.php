29a2294dcadf3a34d6b17c0499fb4129577a5c19
Yudi <madewahyudi22@gmail.com>
2021-02-23 13:36:24 +0800

    added review table to itinerary single

--- resources/views/pages/itinerary/edit.blade.php
+++ resources/views/pages/itinerary/edit.blade.php
@@ -94,6 +94,59 @@
                             name="description">{{ old('description', $itinerary->description) }}</textarea>
                     </div>
                 </section>
+                <section>
+                    <h4>Review</h4>
+                    <hr>
+                    <div class="table-responsive">
+                        <table id="datatables" class="table table-striped table-bordered">
+                            <thead>
+                                <th class="text-center">No</th>
+                                <th class="text-center text-nowrap">Customer Name</th>
+                                <th class="text-center text-nowrap">Itinerary Name</th>
+                                <th class="text-center">Content</th>
+                                <th class="text-center">Rating</th>
+                                <th class="text-center">Added at</th>
+                                <th class="text-center">Action</th>
+                            </thead>
+                            <tbody>
+                                @foreach($itinerary->reviews as $key => $review)
+                                    <tr>
+                                        <td class="text-center">{{ $key + 1 }}</td>
+                                        <td class="text-center">
+                                            <a class="text-dark field-hover" href="{{ route('customer.edit', $review->customer) }}"
+                                                                             title="Edit customer {{ $review->customer->name }}">
+                                                                             {{ $review->customer->name }}
+                                            </a>
+                                        </td>
+                                        <td class="text-center">
+                                            <a class="text-dark field-hover" href="{{ route('itinerary.edit', $review->itinerary) }}"
+                                                                             title="Edit itinerary {{ $review->itinerary->place_name }}">
+                                                                             {{ $review->itinerary->place_name }}
+                                            </a>
+                                        </td>
+                                        <td class="text-center text-truncate">
+                                            <a class="text-dark" href="{{ route('review.edit', $review) }}" title="Edit this review">
+                                                {{ $review->content }}
+                                            </a>
+                                        </td>
+                                        <td class="text-center">{{ $review->rating }}</td>
+                                        <td class="text-center">{{ $review->created_at->diffForHumans() }}</td>
+                                        <td class="text-center text-nowrap align-middle">
+                                            <a class="btn btn-primary btn-sm" href="{{ route('review.edit', $review) }}"
+                                                                              title="Edit">
+                                                <i class="fa fa-fw fa-pencil-alt"></i>
+                                            </a>
+                                            <button type="button" onclick="handleDelete({{ $review->id }})"
+                                                                  class="btn btn-sm btn-danger">
+                                                                  <i class="fa fa-fw fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
             </div>
             <div class="col-lg-4">
                 <section>

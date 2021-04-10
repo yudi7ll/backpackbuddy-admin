74e5b98af23aa89aaba760c682128ee4a12d8a73
Yudi <madewahyudi22@gmail.com>
2021-04-10 09:26:09 +0800

    minor

--- app/Http/Traits/MediaTrait.php
+++ app/Http/Traits/MediaTrait.php
@@ -32,7 +32,7 @@ trait MediaTrait
         $media['url'] = Storage::disk('public')->url("{$media['type']}/{$media['name']}");
         $media['thumbnail_url'] = Storage::disk('public')->url("{$media['type']}/thumb/{$media['name']}");
         $media['file_size'] = $file->getSize();
-        $media['alt'] = "{$fieldname}";
+        $media['alt'] = $fieldname;
 
         return $media;
     }

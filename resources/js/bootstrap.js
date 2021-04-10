10d746768604a65187f684dba0a555d07960d2c8
Yudi <madewahyudi22@gmail.com>
2021-04-10 10:27:14 +0800

    minor

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -7,17 +7,17 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+    window.Popper = require("popper.js").default;
+    window.$ = window.jQuery = require("jquery");
 
-    require('bootstrap');
+    require("bootstrap");
 
-    require('datatables.net-bs4');
-    require('icheck-bootstrap');
-    require('overlayscrollbars');
-    require('select2');
-    require('sweetalert');
-    require('../../public/vendor/adminlte/dist/js/adminlte.min');
+    require("datatables.net-bs4");
+    require("icheck-bootstrap");
+    require("overlayscrollbars");
+    require("select2");
+    require("sweetalert");
+    require("../../public/vendor/adminlte/dist/js/adminlte.min");
 } catch (e) {}
 
 /**
@@ -26,9 +26,9 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening

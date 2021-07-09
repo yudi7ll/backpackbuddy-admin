86a893a5a0d599a7e87c7fa11d19b952f550581a
Yudi <madewahyudi22@gmail.com>
2021-07-09 18:39:08 +0800

    update fresh.sh script

--- bin/fresh.sh
+++ bin/fresh.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
-docker-compose exec php php artisan migrate:fresh --seed
-docker-compose exec php php artisan passport:install --force
+lando artisan migrate:fresh --seed
+lando artisan passport:install --force
 rm -rf public/storage/Itinerary
 mkdir -p public/storage/Itinerary/thumb
 cp -rf public/storage/samples/* public/storage/Itinerary/
